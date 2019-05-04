58445819c17e4e6dfe64ee44ec1f050cf6d14c75
umesh lakshan <dglumesh@gmail.com>
2019-05-04 23:28:59 +0530

    Enrollment DONE

--- ContosoUniversity/Controllers/EnrollmentController.cs
+++ ContosoUniversity/Controllers/EnrollmentController.cs
@@ -1,11 +1,12 @@
 ﻿using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
-using ContosoUniversity.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
 
 namespace ContosoUniversity.Controllers
 {
@@ -15,32 +16,82 @@ namespace ContosoUniversity.Controllers
         // GET: Enrollment
         public ActionResult Index()
         {
-            return View(db.Enrollments.ToList());
+            
+            var enrollment = db.Enrollments.Include(e => e.Course).Include(e =>e.Student);
+            return View(enrollment.ToList());
         }
-        public ActionResult New()
+        public ActionResult Create()
         {
-            var course = db.Courses.ToList();
-            var student = db.Students.ToList();
-            var viewModel = new StudentCourseViewModel
-            {
-                Course = course,
-                Student =student
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName");
+         
 
-            };
-        
 
-            return View("EnrollmentForm", viewModel);
+            return View();
         }
         [HttpPost]
-        public ActionResult Save(Student student,Course course)
+        public ActionResult Create(Enrollment enrollment)
         {
-            db.Students.Add(student);
+            db.Enrollments.Add(enrollment);
             db.SaveChanges();
 
-            db.Courses.Add(course);
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName", enrollment.StudentID);
+
+            return RedirectToAction("Index");
+        }
+       public ActionResult Edit(int? id)
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "LastName");
+
+            Enrollment enrollment = db.Enrollments.Find(id);
+
+            return View(enrollment);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult EditPost(Enrollment enrollment)
+        {
+            db.Entry(enrollment).State = EntityState.Modified;
             db.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
